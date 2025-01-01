07df60380c9547394a79216dc7d4450a7b75a9e7
siddhi <87372462+sid161122@users.noreply.github.com>
2025-01-01 16:19:23 +0530

    Update curry.js

--- curry.js
+++ curry.js
@@ -12,11 +12,11 @@ const step3 = step2("peppers");
 
 console.log(step3)
 
-
+//it helps us to avoid passing the same variable multiple times
 //It helps us to create a higher-order function(function that makes or uses other function)
 function createhero(hero) {  //base - so spiderman is one but powers r many
     return function (power) {
-        return function (power1) { 
+        return function (power1) {
 
             return `${hero} ${power1} ${power}`
         }
@@ -26,6 +26,16 @@ function createhero(hero) {  //base - so spiderman is one but powers r many
 const hero = createhero("spiderman") //reuse
 console.log(hero("swing")("skate"))
 // console.log(hero("skate"))
+
+
+//without currying
+function createhero(hero, power, power1) {
+    return `${hero} ${power1} ${power}`;
+}
+
+console.log(createhero('spider', 'stick', 'save')); // 105
+console.log(createhero('spider', 'swing', 'canon event')); // 210
+
 
 //Reusability: Pre-configure parts of a function for specific contexts.
 // Modularity: Break functions into smaller, composable pieces.
